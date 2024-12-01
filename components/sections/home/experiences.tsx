63c5214b384637dc34f4067588719bc97a4c392c
Samuel Costa <samuel.costa@ccc.ufcg.edu.br>
2024-12-01 15:25:17 -0300

    adicionando informações

--- components/sections/home/experiences.tsx
+++ components/sections/home/experiences.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image';
+
+//components ui
 import { Badge } from "@/components/ui/badge";
-import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+//icons
 import { FaRegCalendarAlt } from "react-icons/fa";
+
+//certifications
+import portfolio from "../../../public/assets/certificates/course-certificates-SCRUMstudy_Samuel_Ribeiro_Costa.png";
 
 interface ExperienceProps {
   title: string;
@@ -12,16 +22,20 @@ interface ExperienceProps {
   regime: string;
   children: React.ReactNode;
   description: string;
+  link?: string;
+  declaration?: any;
 }
 
-function Experience({title, place, firstMonth, secondMonth, regime, children, description}: ExperienceProps) {
+function Experience({title, place, firstMonth,
+  secondMonth, regime, children, description,
+  link, declaration}: ExperienceProps) {
   return (
-    <div className="mb-6">
-      <h1 className="text-neutral-950 text-2xl font-bold">
+    <div className="mb-6 flex flex-col">
+      <h1 className="text-neutral-950 text-2xl font-semibold">
         {title}
       </h1>
 
-      <h2 className="text-indigo-600 font-bold">
+      <h2 className="text-indigo-600 font-semibold">
         {place}
       </h2>
 
@@ -32,206 +46,254 @@ function Experience({title, place, firstMonth, secondMonth, regime, children, de
         {regime}
       </div>
 
-      <div className="flex flex-wrap gap-2 mt-2 mb-4">
+      <div className="flex flex-wrap gap-2 mt-2">
         {children}
       </div>
               
-      <span>
+      <span className="my-4 font-light">
         {description}
       </span>
+
+      {link &&
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-fit"
+        >
+          <Button>Acessar</Button>
+        </a>
+      }
+
+      {declaration &&
+        <Dialog>
+          <DialogTrigger className="w-fit">
+            <Button>Ver declaração</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <Image
+              width="2000"
+              height="2000"
+              src={declaration}
+              alt="declaração de participação de projeto"
+            />
+          </DialogContent>
+        </Dialog>
+      }
     </div>
   )
 }
 
 export default function Experiences() {
   return (
-    <div className="w-[90vw] p-16 flex flex-col rounded-b-[20px] bg-neutral-50 z-10">
-      <h1 className="text-neutral-950 text-4xl font-bold mb-8">
-        Experiências
-      </h1>
+    <Card className="w-[90vw] flex flex-col rounded-t-none rounded-b-[20px] border-none bg-neutral-50 z-10">
+      <CardHeader className="rounded-t-none">
+        <h1 className="text-neutral-950 text-4xl font-bold mb-8">
+          Experiências
+        </h1>
+      </CardHeader>
 
-      <Tabs defaultValue="2021" className="flex" orientation="vertical">
-        <TabsList className="flex flex-col h-fit bg-indigo-100">
-          <TabsTrigger
-            value="2021"
-            className="data-[state=active]:bg-indigo-600 data-[state=active]:text-white text-indigo-400 hover:bg-indigo-200 focus:bg-indigo-200"
-          >
-            2021
-          </TabsTrigger>
-          <TabsTrigger
-            value="2022"
-            className="data-[state=active]:bg-indigo-600 data-[state=active]:text-white text-indigo-400 hover:bg-indigo-200 focus:bg-indigo-200"
-          >
-            2022
-          </TabsTrigger>
-          <TabsTrigger
-            value="2023"
-            className="data-[state=active]:bg-indigo-600 data-[state=active]:text-white text-indigo-400 hover:bg-indigo-200 focus:bg-indigo-200"
-          >
-            2023
-          </TabsTrigger>
-          <TabsTrigger
-            value="2024"
-            className="data-[state=active]:bg-indigo-600 data-[state=active]:text-white text-indigo-400 hover:bg-indigo-200 focus:bg-indigo-200"
-          >
-            2024
-          </TabsTrigger>
-        </TabsList>
-
-        <TabsContent value="2021" className="w-full">
-          <Card className="h-[650px] p-6 ml-4 border-indigo-100">
-            <Experience
-              title="Fullstack trainee"
-              place="Laboratório de Engenharia de Pavimentos (LEP) - Universidade Federal de Campina Grande (Remoto)"
-              firstMonth="Julho"
-              secondMonth="Dezembro"
-              regime="Meio período"
-              description="aaa"
+      <CardContent>
+        <Tabs defaultValue="2021" className="flex" orientation="vertical">
+          <TabsList className="flex flex-col h-fit bg-indigo-100">
+            <TabsTrigger
+              value="2021"
+              className="data-[state=active]:bg-indigo-600 data-[state=active]:text-white text-indigo-400 hover:bg-indigo-200 focus:bg-indigo-200"
             >
-              <Badge>FullStack</Badge>
-              <Badge>React.js</Badge>
-              <Badge>Node.js</Badge>
-              <Badge>Javascript</Badge>
-              <Badge>Typescript</Badge>
-              <Badge>Styled-Components</Badge>
-            </Experience>
-
-            <Experience
-              title="Estudante PD&I"
-              place="Software Practices Laboratory (SPLab) - Universidade Federal de Campina Grande (Remoto)"
-              firstMonth="Julho"
-              secondMonth="Setembro"
-              regime="Meio período"
-              description="aaa"
+              2021
+            </TabsTrigger>
+            <TabsTrigger
+              value="2022"
+              className="data-[state=active]:bg-indigo-600 data-[state=active]:text-white text-indigo-400 hover:bg-indigo-200 focus:bg-indigo-200"
             >
-              <Badge>Ciência de dados</Badge>
-              <Badge>Aprendizagem de máquina</Badge>
-              <Badge>Python</Badge>
-              <Badge>Pandas</Badge>
-              <Badge>Sklearn</Badge>
-              <Badge>Jupyter notebook</Badge>
-            </Experience>
-
-            <Experience
-              title="Estudante de Capacitação"
-              place="Embedded + VIRTUS - Universidade Federal de Campina Grande (Remoto)"
-              firstMonth="Outubro"
-              secondMonth="Dezembro"
-              regime="Meio período"
-              description="aaa"
+              2022
+            </TabsTrigger>
+            <TabsTrigger
+              value="2023"
+              className="data-[state=active]:bg-indigo-600 data-[state=active]:text-white text-indigo-400 hover:bg-indigo-200 focus:bg-indigo-200"
             >
-              <Badge>Testes automatizados</Badge>
-              <Badge>Javascript</Badge>
-              <Badge>Node.js</Badge>
-              <Badge>Cypress</Badge>
-            </Experience>
-          </Card>
-        </TabsContent>
-
-        <TabsContent value="2022" className="w-full">
-          <Card className="h-[650px] p-6 ml-4 border-indigo-200">
-            <Experience
-              title="Desenvolvedor fullstack"
-              place="Laboratório de Engenharia de Pavimentos (LEP) - Universidade Federal de Campina Grande (Remoto)"
-              firstMonth="Janeiro"
-              secondMonth="Dezembro"
-              regime="Meio período"
-              description="aaa"
+              2023
+            </TabsTrigger>
+            <TabsTrigger
+              value="2024"
+              className="data-[state=active]:bg-indigo-600 data-[state=active]:text-white text-indigo-400 hover:bg-indigo-200 focus:bg-indigo-200"
             >
-              <Badge>Teste automatizados</Badge>
-            </Experience>
+              2024
+            </TabsTrigger>
+          </TabsList>
 
-            <Experience
-              title="Estudante de Capacitação"
-              place="Embedded + VIRTUS - Universidade Federal de Campina Grande (Remoto)"
-              firstMonth="Janeiro"
-              secondMonth="Março"
-              regime="Meio período"
-              description="aaa"
-            >
-              <Badge>Testes automatizados</Badge>
-              <Badge>Javascript</Badge>
-              <Badge>Node.js</Badge>
-              <Badge>Cypress</Badge>
-            </Experience>
+          <TabsContent value="2021" className="w-full">
+            <Card className="h-[800px] p-6 ml-4 border-indigo-100">
+              <Experience
+                title="Fullstack trainee"
+                place="Laboratório de Engenharia de Pavimentos (LEP) - Universidade Federal de Campina Grande (Remoto)"
+                firstMonth="Julho"
+                secondMonth="Dezembro"
+                regime="Meio período"
+                description="Neste projeto do LEP em conjunto com a JRB Engenharia pude aprender a utilizar 
+                React e Node para desenvolver uma solução real com usuários reais pela primeira vez, o 
+                FASTENG. O projeto consiste em uma plataforma para anotações e realizações de cálculos na 
+                área de Engenharia Civil. Na plataforma é possível cadastrar amostras coletadas de materiais
+                de asfalto e solos e realizar ensaios seguindo as regras da DNIT Inicialmente
+                tive a responsabilidade de corrigir alguns erros de Javascript e na responsividade das telas."
+                link="https://smartdoser.fastengapp.com.br/"
+              >
+                <Badge>FullStack</Badge>
+                <Badge>React.js</Badge>
+                <Badge>Node.js</Badge>
+                <Badge>Express</Badge>
+                <Badge>Axios</Badge>
+                <Badge>Javascript</Badge>
+                <Badge>Typescript</Badge>
+                <Badge>Styled-Components</Badge>
+                <Badge>SCRUM</Badge>
+                <Badge>Trello</Badge>
+                <Badge>Github</Badge>
+              </Experience>
 
-            <Experience
-              title="Web Designer e Desenvolvedor Frontend"
-              place="Laboratório de Computação Inteligente Aplicada (LACINA) - Universidade Federal de Campina Grande (Remoto)"
-              firstMonth="Agosto"
-              secondMonth="Dezembro"
-              regime="Meio período"
-              description="aaa"
-            >
-              <Badge>Testes automatizados</Badge>
-              <Badge>Javascript</Badge>
-              <Badge>Node.js</Badge>
-              <Badge>Cypress</Badge>
-            </Experience>
-          </Card>
-        </TabsContent>
+              <Experience
+                title="Estudante PD&I"
+                place="Software Practices Laboratory (SPLab) - Universidade Federal de Campina Grande (Remoto)"
+                firstMonth="Julho"
+                secondMonth="Setembro"
+                regime="Meio período"
+                description="Neste projeto de Pesquisa, Desenvolvimento e Inovação, que foi uma parceria entre
+                o SPLab e a empresa Siteware. Inicialmente me foi disponibilizado alguns minicursos sobre
+                análise de dados. Realizamos algumas reuniões de alinhamento com o representante da Siteware.
+                Então, começamos a tratar e produzir visualizações com os dados que tínhamos para entender
+                melhor o contexto. Depois dessa visualização inicial, iniciamos o processo de realizar predições
+                com os dados tratados."
+                declaration={portfolio}
+              >
+                <Badge>Ciência de dados</Badge>
+                <Badge>Aprendizagem de máquina</Badge>
+                <Badge>Python</Badge>
+                <Badge>Pandas</Badge>
+                <Badge>Sklearn</Badge>
+                <Badge>Jupyter notebook</Badge>
+              </Experience>
 
-        <TabsContent value="2023" className="w-full">
-          <Card className="h-[650px] p-6 ml-4 border-indigo-200">
-            <Experience
-              title="Desenvolvedor fullstack"
-              place="Laboratório de Engenharia de Pavimentos (LEP) - Universidade Federal de Campina Grande (Remoto)"
-              firstMonth="Janeiro"
-              secondMonth="Setembro"
-              regime="Meio período"
-              description="aaa"
-            >
-              <Badge>Teste automatizados</Badge>
-            </Experience>
+              <Experience
+                title="Estudante de Capacitação"
+                place="Embedded + VIRTUS - Universidade Federal de Campina Grande (Remoto)"
+                firstMonth="Outubro"
+                secondMonth="Dezembro"
+                regime="Meio período"
+                description="aaa"
+              >
+                <Badge>Testes automatizados</Badge>
+                <Badge>Javascript</Badge>
+                <Badge>Node.js</Badge>
+                <Badge>Cypress</Badge>
+              </Experience>
+            </Card>
+          </TabsContent>
 
-            <Experience
-              title="Web Designer e Desenvolvedor Frontend"
-              place="Laboratório de Computação Inteligente Aplicada (LACINA) - Universidade Federal de Campina Grande (Remoto)"
-              firstMonth="Janeiro"
-              secondMonth="Março"
-              regime="Meio período"
-              description="aaa"
-            >
-              <Badge>Testes automatizados</Badge>
-              <Badge>Javascript</Badge>
-              <Badge>Node.js</Badge>
-              <Badge>Cypress</Badge>
-            </Experience>
+          <TabsContent value="2022" className="w-full">
+            <Card className="h-[650px] p-6 ml-4 border-indigo-200">
+              <Experience
+                title="Desenvolvedor fullstack"
+                place="Laboratório de Engenharia de Pavimentos (LEP) - Universidade Federal de Campina Grande (Remoto)"
+                firstMonth="Janeiro"
+                secondMonth="Dezembro"
+                regime="Meio período"
+                description="aaa"
+              >
+                <Badge>Teste automatizados</Badge>
+              </Experience>
 
-            <Experience
-              title="Engenheiro de Software"
-              place="ServeSoft"
-              firstMonth="Outubro"
-              secondMonth="Dezembro"
-              regime="Integral"
-              description="aaa"
-            >
-              <Badge>Testes automatizados</Badge>
-              <Badge>Javascript</Badge>
-              <Badge>Node.js</Badge>
-              <Badge>Cypress</Badge>
-            </Experience>
-          </Card>
-        </TabsContent>
+              <Experience
+                title="Estudante de Capacitação"
+                place="Embedded + VIRTUS - Universidade Federal de Campina Grande (Remoto)"
+                firstMonth="Janeiro"
+                secondMonth="Março"
+                regime="Meio período"
+                description="aaa"
+              >
+                <Badge>Testes automatizados</Badge>
+                <Badge>Javascript</Badge>
+                <Badge>Node.js</Badge>
+                <Badge>Cypress</Badge>
+              </Experience>
 
-        <TabsContent value="2024" className="w-full">
-          <Card className="h-[650px] p-6 ml-4 border-indigo-200">
-            <Experience
-              title="Engenheiro de Software"
-              place="ServeSoft"
-              firstMonth="Outubro"
-              secondMonth="Dezembro"
-              regime="Integral"
-              description="aaa"
-            >
-              <Badge>Testes automatizados</Badge>
-              <Badge>Javascript</Badge>
-              <Badge>Node.js</Badge>
-              <Badge>Cypress</Badge>
-            </Experience>
-          </Card>
-        </TabsContent>
-      </Tabs>
-    </div>
+              <Experience
+                title="Web Designer e Desenvolvedor Frontend"
+                place="Laboratório de Computação Inteligente Aplicada (LACINA) - Universidade Federal de Campina Grande (Remoto)"
+                firstMonth="Agosto"
+                secondMonth="Dezembro"
+                regime="Meio período"
+                description="aaa"
+              >
+                <Badge>Testes automatizados</Badge>
+                <Badge>Javascript</Badge>
+                <Badge>Node.js</Badge>
+                <Badge>Cypress</Badge>
+              </Experience>
+            </Card>
+          </TabsContent>
+
+          <TabsContent value="2023" className="w-full">
+            <Card className="h-[650px] p-6 ml-4 border-indigo-200">
+              <Experience
+                title="Desenvolvedor fullstack"
+                place="Laboratório de Engenharia de Pavimentos (LEP) - Universidade Federal de Campina Grande (Remoto)"
+                firstMonth="Janeiro"
+                secondMonth="Setembro"
+                regime="Meio período"
+                description="aaa"
+              >
+                <Badge>Teste automatizados</Badge>
+              </Experience>
+
+              <Experience
+                title="Web Designer e Desenvolvedor Frontend"
+                place="Laboratório de Computação Inteligente Aplicada (LACINA) - Universidade Federal de Campina Grande (Remoto)"
+                firstMonth="Janeiro"
+                secondMonth="Março"
+                regime="Meio período"
+                description="aaa"
+              >
+                <Badge>Testes automatizados</Badge>
+                <Badge>Javascript</Badge>
+                <Badge>Node.js</Badge>
+                <Badge>Cypress</Badge>
+              </Experience>
+
+              <Experience
+                title="Engenheiro de Software"
+                place="ServeSoft"
+                firstMonth="Outubro"
+                secondMonth="Dezembro"
+                regime="Integral"
+                description="aaa"
+              >
+                <Badge>Testes automatizados</Badge>
+                <Badge>Javascript</Badge>
+                <Badge>Node.js</Badge>
+                <Badge>Cypress</Badge>
+              </Experience>
+            </Card>
+          </TabsContent>
+
+          <TabsContent value="2024" className="w-full">
+            <Card className="h-[650px] p-6 ml-4 border-indigo-200">
+              <Experience
+                title="Engenheiro de Software"
+                place="ServeSoft"
+                firstMonth="Outubro"
+                secondMonth="Dezembro"
+                regime="Integral"
+                description="aaa"
+              >
+                <Badge>Testes automatizados</Badge>
+                <Badge>Javascript</Badge>
+                <Badge>Node.js</Badge>
+                <Badge>Cypress</Badge>
+              </Experience>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </CardContent>
+    </Card>
   )
 }
--- components/sections/home/introduce.tsx
+++ components/sections/home/introduce.tsx
@@ -1,8 +1,7 @@
 import { buttonVariants } from "@/components/ui/button";
 import WordRotate from "@/components/ui/word-rotate";
 import Link from "next/link";
-import { GrFormNext, GrFormPrevious } from "react-icons/gr";
-import { RxSlash } from "react-icons/rx";
+import { GrFormNext } from "react-icons/gr";
 
 export default function Introduce() {
   return (
@@ -11,11 +10,11 @@ export default function Introduce() {
         <div className="flex text-6xl font-semibold text-neutral-50">
           {/* <GrFormPrevious className="text-indigo-600"/> */}
           {/*<span className="text-indigo-600">{"<"}</span>*/} Samuel Costa {/*<span className="text-indigo-600">{" />"}</span>*/}
-          <GrFormNext className="text-indigo-600"/>
+          {/* <GrFormNext className="text-indigo-600"/> */}
         </div>
         
         <WordRotate
-          className="text-4xl text-teal-400 font-thin"
+          className="text-4xl text-teal-400 font-thin mb-2"
           words={["Desenvolvedor Frontend", "Web Designer", "Engenheiro de Software"]}
         />
 
@@ -24,10 +23,10 @@ export default function Introduce() {
         </Link>
       </div>
 
-      <div className="h-[340px] w-[370px] relative">
-        <div className="h-[300px] w-[300px] rounded-[20px] bg-neutral-800/10 backdrop-blur-2xl absolute bottom-0 left-[40px] z-[6]" />
-        <div className="h-[240px] w-[240px] rounded-[20px] bg-indigo-600 absolute top-0 left-0 z-[5]"/>
-        <div className="h-[150px] w-[150px] rounded-[20px] bg-teal-400 absolute top-1/3 right-0 z-[4]"/>
+      <div className="h-[440px] w-[470px] relative">
+        <div className="h-[400px] w-[400px] rounded-[20px] bg-neutral-800/10 backdrop-blur-2xl absolute bottom-0 left-[40px] z-[6]" />
+        <div className="h-[300px] w-[300px] rounded-[20px] bg-indigo-600 absolute top-0 left-0 z-[5]"/>
+        <div className="h-[200px] w-[200px] rounded-[20px] bg-teal-400 absolute top-1/3 right-0 z-[4]"/>
       </div>
     </div>
   )
